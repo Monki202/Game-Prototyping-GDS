ed608ef7ff498a2764b321f39f2503a9a74cae87
Monki202 <90990275+Monki202@users.noreply.github.com>
2026-05-07 14:36:46 -0400

    5/7/2026

--- js/GameLoop.js
+++ js/GameLoop.js
@@ -1,125 +1,44 @@
-var canvas;
-var context;
-var player;
-var timer;
-var interval = 1000/60;
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d")
 
-var frictionX = 0.8;
-var frictionY = 0.8;
-var gravity = 1;
+var interval = 1000/60
+var timer = setInterval(animate, interval);
 
-canvas = document.getElementById("canvas");
-context = canvas.getContext("2d");
+var colors = [];
+colors[0] = "#ff0000";
+colors[1] = "#00ff00";
+colors[2] = "#0000ff";
 
-player = new GameObject(100,canvas.height/2,100,100,"#eeea1e");
-platform0 = new GameObject();
-platform0.width = 300;
-platform0.y = player.y + player.height/2 + platform0.height/2;
-platform0.color = "#2600ff";
+var amt = 50;
 
-timer = setInterval(animate, interval);
+dots.vx = 10;
+dots.vy = 10;
+
+var dots = [];
+for(var i = 0; i < amt; i++)
+{
+    dots[i] = new GameObject();
+    dots[i].x = Math.random() * canvas.width;
+    dots[i].y = Math.random() * canvas.height;
+    dots[i].color = colors[Math.floor(rand(0,2.9))];
+    dots[i].width = rand(5,20);
+}
 
 function animate()
 {
-    context.clearRect(0,0,canvas.width,canvas.height);
+    context.clearRect(0,0,canvas.clientWidth,canvas.height);
 
-    if (w && player.canJump)
+    for(var i = 0; i < amt; i++)
     {
-        player.canJump = false;
-        player.vy += player.jumpSpeed;
+        dots[i].drawCircle();
     }
 
-    doHandleAcceleration();
-    doHandleFriction();
-    doHandleGravity();
-    doUpdatePosition();
-    doCheckBottomBounds();
+    dots.move();
 
-    while(platform0.hitTestPoint(player.bottom()) && player.vy >= 0)
-    {
-        player.y--;
-        player.vy = 0;
-        player.canJump = true;
-    }
 
-    // while(platform0.hitTestPoint(player.top()))
-    // {
-    //     player.y++;
-    //     player.vy = 0;
-    // }
-
-    player.move();
-    if (player.x > canvas.width + player.width/2)
-    {
-       // player.vx *= -1;
-        player.color = "#40ff00";
-    }
-    if (player.x < 0 + player.width/2)
-    {
-       // player.vx = 2;
-        player.color = "#ff0000";
-    }
-    if (player.y > canvas.height + player.height/2)
-    {
-        //player.vy *= -1;
-        player.color = "#ff40b6";
-    }
-    if (player.y < 0 + player.height/2)
-    {
-       // player.vy = 2;
-        player.color = "#0000ff";
-    }
-
-    player.jumpSpeed = -20
-
-    player.drawCircle();
-    player.drawDebug();
-    platform0.drawRect();
 }
 
-function doHandleAcceleration()
+function rand(low, high)
 {
-    if (d)
-    {
-        player.vx += player.ax * player.force;
-    }
-    if (a)
-    {
-        player.vx += player.ax * -player.force;
-    }
-}
-
-function doHandleFriction()
-{
-    player.vx *= frictionX;
-}
-
-function doHandleGravity()
-{
-    player.vy += gravity;
-}
-
-function doUpdatePosition()
-{
-    player.x += player.vx;
-    player.y += player.vy;
-}
-
-function doCheckBottomBounds()
-{
-    if (player.y > canvas.height - player.height/2)
-    {
-        player.y = canvas.height - player.height/2;
-        player.vy = 0;
-        player.canJump = true;
-    }
-}
-
-function doJump()
-{
-    if (w)
-    {
-        player.vy = player.jumpSpeed;
-        
-    }
+    return Math.random() * (high - low) + low;
 }
